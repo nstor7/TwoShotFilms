dcdfb036b4e386676c353c7e33ca05b2d13a186f
Nestor Ibarra <nestoribarra@iMac-de-Nestor.local>
2025-12-02 20:19:21 -0500

    Committer: Nestor Ibarra <nestoribarra@iMac-de-Nestor.local>
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   src/header.jsx
    	modified:   src/header.module.css
    	modified:   src/paginas/servicio.module.css

--- src/header.jsx
+++ src/header.jsx
@@ -1,45 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import {Servicios} from './DB/servicios';
 import styles from './header.module.css';
 import Social from './components/SocialLinks'
-// {servicios.map(servicio => (
-//   <li key={servicio.id}>
-//     <NavLink
-//       to={`/${servicio.h1.toLowerCase().replace(/\s+/g, '-')}`}
-//       className={styles.navLink}
-//       activeClassName={styles.active}
-//     >
-//       {servicio.h1.split(" en Panamá")[0]}
-//     </NavLink>
-//   </li>
-// ))}
+import { FaBars }  from 'react-icons/fa'
+
 const Header = () => {
-  // const [isScrolled, setIsScrolled] = useState(false);
+  var [visible, setVisible] = useState(false)
 
-  // // Escucha el evento de scroll y actualiza el estado
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const scrolled = window.scrollY > 200; // Cambia a blanco después de 50px de scroll
-  //     setIsScrolled(scrolled);
-  //   };
+  var mostrarMenu = ()=>{
+    setVisible(!visible)
+  }
+  
 
-  //   window.addEventListener('scroll', handleScroll);
-  //   return () => window.removeEventListener('scroll', handleScroll); // Limpieza
-  // }, []);
-
-  return (
+  var  classNameSocial = visible ? styles.social : styles.socialNone
+  var classNameNav = visible ? styles.navList :  styles.navListNone 
+    return (
     <header className={styles.header}>
       <div className={styles.logo}><img className={styles.logo} src="/images/Logo TwoShot.png" alt="" /></div>
       <nav className={styles.nav}>
-        <ul className={styles.navList}>
+        <ul className={classNameNav}>
           <li><NavLink to="/" className={styles.navLink} >Inicio</NavLink></li>
           <li><NavLink to="/servicios" className={styles.navLink} >Servicios</NavLink></li>
           
           <li><NavLink to="/portfolio" className={styles.navLink} >Portafolio</NavLink></li>
         </ul>
       </nav>
-      <Social />
+      <div className={classNameSocial} > <Social /></div>
+     <div className={styles.bars} onClick={mostrarMenu}> <FaBars /> </div>
     </header>
   );
 };
